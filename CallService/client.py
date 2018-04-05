6f9e4ee0f70c436724ad5705e9440480e9341508
srinskit <srinagrao2007@gmail.com>
2018-04-05 19:37:18 +0530

    Fixes

--- CallService/client.py
+++ CallService/client.py
@@ -5,9 +5,11 @@ from time import sleep
 
 incoming_call_port = 8000
 server_port = 8001
-server_ip = '127.0.0.1'
+server_ip = '10.50.18.161'
 incoming_call_thread, outgoing_call_thread = None, None
 expecting_call_back_from = None
+run = True
+recv_sock = None
 
 
 def msg_routine():
@@ -16,7 +18,7 @@ def msg_routine():
 
 def read_sock(conn):
     data = b''
-    while True:
+    while run:
         block = conn.recv(10)
         data += block
         if block.find(b'\n') >= 0:
@@ -56,14 +58,16 @@ def ipr(ip):
 
 
 def listen_call():
-    global expecting_call_back_from, outgoing_call_thread
+    global expecting_call_back_from, outgoing_call_thread, run, recv_sock
     recv_sock = socket.socket()
     recv_sock.bind(('', incoming_call_port))
     recv_sock.listen(1)
     conn = None
     try:
-        while True:
+        while run:
             conn, address = recv_sock.accept()
+            if not run:
+                break
             username = unr(address[0])
             if expecting_call_back_from is not None and expecting_call_back_from != username:
                 conn.close()
@@ -105,6 +109,7 @@ def call(username):
 
 
 def main(username, password):
+    global run
     msg_sock = socket.socket()
     msg_sock.connect((server_ip, server_port))
     if auth(msg_sock, username, password):
@@ -112,14 +117,15 @@ def main(username, password):
     else:
         print('Auth Failure')
     msg_sock.close()
-    # call_thread = threading.Thread(target=listen_call)
-    # call_thread.start()
-    # call_thread.join()
+    call_thread = threading.Thread(target=listen_call)
+    call_thread.start()
     try:
         while True:
             x = input("# ")
             y = x.split()
             if y[0] == 'exit':
+                run = False
+                recv_sock.close()
                 break
             elif y[0] == 'call':
                 outgoing_call_thread = threading.Thread(target=call, kwargs={'username': y[1]})
@@ -130,6 +136,7 @@ def main(username, password):
                 print(unr(y[1]))
     except KeyboardInterrupt:
         pass
+    call_thread.join()
     print()
 
 
