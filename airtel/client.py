774b0cf7f075c21cf51fb1156582463072c25cc2
Sanjay <sanjayshetty2015@gmail.com>
2018-04-08 14:33:08 +0530

    groupchat

--- airtel/client.py
+++ airtel/client.py
@@ -1,18 +1,28 @@
 import sys
+import select
+import socket         
 def main(username):
-    import socket          
     s = socket.socket()          
-    port = 12344             
+    port = 12345           
     s.connect(('10.50.19.210', port))
     print("enter $ to disconnect")
     flag=True
     while(True):
-        msg=input()
-        if(msg=='$'):
-            break
-        s.send((username+":").encode("ascii"))
-        s.send((msg).encode("ascii"))
-        print(s.recv(1024))
+        sockets_list = [sys.stdin, s]
+        read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+        print(read_sockets)
+        for socks in read_sockets:
+            if socks == s:
+                print("true")
+                message = socks.recv(2048)
+                print(message)
+            else:
+                print("false")
+                message = sys.stdin.readline()
+                s.send(message.encode("ascii"))
+                sys.stdout.write("<You>")
+                sys.stdout.write(message)
+                sys.stdout.flush()
     s.close()       
 if __name__ == '__main__':
     if(len(sys.argv))!=2:
--- airtel/server.py
+++ airtel/server.py
@@ -1,18 +1,58 @@
-# first of all import the socket library
+import threading    
+list_of_clients = []
+ 
+def clientthread(conn, addr):
+    conn.send(("Welcome to this chatroom!").encode("ascii"))
+    while True:
+        try:
+            message = conn.recv(2048)
+            print(message)
+            if message:
 
+                """prints the message and address of the
+                user who just sent the message on the server
+                terminal"""
+                print("<" + addr[0] + "> " + message)
+
+                # Calls broadcast function to send message to all
+                message_to_send = "<" + addr[0] + "> " + message
+                broadcast(message_to_send, conn)
+
+            else:
+                """message may have no content if the connection
+                is broken, in this case we remove the connection"""
+                remove(conn)
+
+        except:
+            continue
+def broadcast(message, connection):
+    for clients in list_of_clients:
+        if clients!=connection:
+            try:
+                clients.send(message.encode("ascii"))
+            except:
+                clients.close()
+ 
+                # if the link is broken, we remove the client
+                remove(clients)
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
 def main():
     import socket               
     s = socket.socket()         
     print("Socket successfully created") 
-    port = 12344             
+    port = 12345           
     s.bind(('10.50.19.210', port))        
     print("socket binded to %s" %(port))
     s.listen(5)     
     print("socket is listening")            
     while True:
-       c, addr = s.accept()
-       print(c.recv(1024))     
-       c.send(("Thank you for connecting").encode("ascii"))
+        conn, addr = s.accept()
+        list_of_clients.append(conn)
+        t=threading.Thread(target=clientthread,args=(conn,addr))
+        t.start() 
     c.close()
+    s.close()
 if __name__ == '__main__':
     main()
