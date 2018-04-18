2e940cadf6a888f4a2b25fe64072c10513aa30ff
Yash Dodeja <ydodeja365@gmail.com>
2018-04-18 19:13:27 +0530

    Private chat functional

--- yash_basic_chat/client.py
+++ yash_basic_chat/client.py
@@ -5,35 +5,39 @@ import sys
  
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
-    print "Correct usage: script, IP address, port number"
+    print("Correct usage: script, IP address, port number")
     exit()
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
 server.connect((IP_address, Port))
+usr=input("Enter username:")
+server.send(usr.encode())
+pwd=input("Enter password:")
+run=True
+server.send(pwd.encode())
+message = server.recv(2048).decode()
+if message == "Invalid Authentication! Connect Again!":
+    run=False
+    print(message)
+else:
+    print(message)
+while run:
+    sockets_list = [sys.stdin,server]
  
-while True:
- 
-    # maintains a list of possible input streams
-    sockets_list = [sys.stdin, server]
- 
-    """ There are two possible input situations. Either the
-    user wants to give  manual input to send to other people,
-    or the server is sending a message  to be printed on the
-    screen. Select returns from sockets_list, the stream that
-    is reader for input. So for example, if the server wants
-    to send a message, then the if condition will hold true
-    below.If the user wants to send a message, the else
-    condition will evaluate as true"""
     read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
  
     for socks in read_sockets:
         if socks == server:
-            message = socks.recv(2048)
-            print message
+            msg=socks.recv(2048).decode()
+            # usr=msg.split()
+            # msg=' '.join(usr[1:])
+            # usr=usr[0]
+            sys.stdout.write(msg)
         else:
+            #sys.stdout.write("Enter message:")
             message = sys.stdin.readline()
-            server.send(message)
+            server.send(message.encode())
             sys.stdout.write("<You>")
             sys.stdout.write(message)
-            sys.stdout.flush()
+
 server.close()
--- yash_basic_chat/new/server.py
+++ yash_basic_chat/new/server.py
@@ -3,6 +3,7 @@ import socket
 import select
 import sys
 from _thread import *
+ 
 """The first argument AF_INET is the address domain of the
 socket. This is used when we have an Internet Domain with
 any two hosts The second argument is the type of socket.
@@ -10,9 +11,10 @@ SOCK_STREAM means that data or characters are read in
 a continuous flow."""
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ 
 # checks whether sufficient arguments have been provided
 if len(sys.argv) != 3:
-    print ("Correct usage: script, IP address, port number")
+    print "Correct usage: script, IP address, port number"
     exit()
  
 # takes the first argument from command prompt as IP address
@@ -36,86 +38,58 @@ server.listen(100)
  
 list_of_clients = []
 list_of_users={'yash':'149','shreyas':'135','sanjay':'136','srinag':'141'}
+ip_resolution={}
 def auth(usr,pwd):
     if usr in list_of_users.keys() and list_of_users[usr] == pwd:
         return True
     return False
-
+ 
 def clientthread(conn, addr):
-    username=conn.recv(2048).decode()
-    password=conn.recv(2048).decode()
+    username=conn.recv(2048)
+    password=conn.recv(2048)
     print("Client entered:"+username,password)
     if not auth(username,password):
         msg='Invalid Authentication! Connect Again!'
         print(msg)
-        conn.send(msg.encode())
+        conn.send(msg)
         conn.close()
         return
     else:
-        list_of_clients.append([username,conn])
         print("Client has authenticated on address",addr[0])
-        
+        ip_resolution[username]=addr[0]
     # sends a message to the client whose user object is conn
-        msg="Welcome to this chatroom!"
-        conn.send(msg.encode())
+        conn.send("Welcome to this chatroom!")
+ 
     while True:
-        try:
-            message = conn.recv(2048).decode()
-            if message:
-                if message[:3]=='\p ':
-                    print("Private mesg..")
-                    print ("<" + username + "> " + message)
-                    user=message[3:].split()
-                    message_to_send='<'+username+'>(Private) '+' '.join(user[1:])+'\n'
-                    user=user[0]
-                    print(user,":",message_to_send)
-                    if not sendPrivate(message_to_send,user):
-                        conn.send("Client not Found!\n".encode())   	
+            try:
+                message = conn.recv(2048)
+                if message:
+                    print "<" + addr[0] + "> " + message+"\nPubMesg"
+                    message_to_send = "<" + addr[0] + "> " + msg
+                    print list_of_clients
+                    broadcast(msg, conn)
+ 
                 else:
-                    print("Public msg")
-                    print ("<" + username + "> " + message)
+                    """message may have no content if the connection
+                    is broken, in this case we remove the connection"""
+                    remove(conn)
  
-                    # Calls broadcast function to send message to all
-                    message_to_send = "<" + username + "> " + message
-                    broadcast(message_to_send, conn)
-
-            else:
-                """message may have no content if the connection
-                is broken, in this case we remove the connection"""
-                remove(conn)
- 
-        except:
-            continue
+            except:
+                continue
  
 """Using the below function, we broadcast the message to all
 clients who's object is not the same as the one sending
 the message """
 def broadcast(message, connection):
     for clients in list_of_clients:
-        if clients[1]!=connection:
+        if clients!=connection:
             try:
-                #print(message)
-                clients[1].send(message.encode())
+                clients.send(message)
             except:
-                clients[1].close()
+                clients.close()
  
                 # if the link is broken, we remove the client
                 remove(clients)
-            break
-def sendPrivate(msg,to):
-    names=[clients[0] for clients in list_of_clients]
-    # print(to in names,names)
-    if to in names:
-        tosock=list_of_clients[names.index(to)][1]
-        #print(tosock)
-        try:
-            tosock.send(msg.encode())
-            print("Sent!")
-        except:
-            tosock.close()
-            remove(tosock)
-    else:
-    	return False
 
 """The following function simply removes the object
 from the list that was created at the beginning of 
@@ -123,15 +97,25 @@ the program"""
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
-def main():
-    while True:
-        conn, addr = server.accept()
+ 
+while True:
+ 
+    """Accepts a connection request and stores two parameters, 
+    conn which is a socket object for that user, and addr 
+    which contains the IP address of the client that just 
+    connected"""
+    conn, addr = server.accept()
+ 
+    """Maintains a list of clients for ease of broadcasting
+    a message to all available people in the chatroom"""
+    list_of_clients.append(conn)
  
     # prints the address of the user that just connected
-        print (addr[0] + " connected")
+    print addr[0] + " connected"
+ 
     # creates and individual thread for every user 
     # that connects
-        start_new_thread(clientthread,(conn,addr))
+    start_new_thread(clientthread,(conn,addr))    
  
-if __name__ == '__main__':
-    main()
+conn.close()
+server.close()
